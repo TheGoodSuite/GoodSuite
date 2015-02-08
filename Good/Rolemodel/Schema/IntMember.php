c917d68f4601be45cb2c0e9014da617f8a97ade4
Jasper Horn <jasperhorn@gmail.com>
2015-02-08 12:23:55 +0100

    Add additional processing to int type modifiers
    - minValue must be lower than or equal to maxValue.
    
    - if nonNegative is set, neither minValue nor maxValue can be negative
    
    - transform nonNegtative into minValue = 0

--- Good/Rolemodel/Schema/IntMember.php
+++ Good/Rolemodel/Schema/IntMember.php
@@ -25,6 +25,29 @@ class IntMember extends PrimitiveMember
     
     function processTypeModifiers(array $typeModifiers)
     {
+        if (array_key_exists('minValue', $typeModifiers) && 
+            array_key_exists('maxValue', $typeModifiers) &&
+            $typeModifiers['minValue'] > $typeModifiers['maxValue'])
+        {
+            throw new \Exception("The minValue for an int cannot be higher than its maxValue");
+        }
+        
+        if (array_key_exists('nonNegative', $typeModifiers))
+        {
+            if ((array_key_exists('minValue', $typeModifiers) && $typeModifiers['minValue'] < 0) ||
+                (array_key_exists('maxValue', $typeModifiers) && $typeModifiers['maxValue'] < 0))
+            {
+                throw new \Exception("If nonNegative is set on an int, its minValue and maxValue can't be negative");
+            }
+            
+            if (!array_key_exists('minValue', $typeModifiers))
+            {
+                $typeModifiers['minValue'] = 0;
+            }
+            
+            unset($typeModifiers['nonNegative']);
+        }
+        
         return $typeModifiers;
     }
     
