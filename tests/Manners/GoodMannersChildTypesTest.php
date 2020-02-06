55da9fe96353c33fcc41829c389c55916e7a52c0
Jasper Horn <jasperhorn@gmail.com>
2020-02-07 00:30:18 +0100

    [#122] Remove mistakenly copied code
    This was always wrong, but is also never executed. In more recent php
    versions however, it started generating a warning...

--- tests/Manners/GoodMannersChildTypesTest.php
+++ tests/Manners/GoodMannersChildTypesTest.php
@@ -87,10 +87,6 @@ class IndependentFactoryBoth implements \Good\Manners\StorableFactory
         {
             return new ChildType2();
         }
-        else
-        {
-            return parent::createStorable($type);
-        }
     }
 }
 
