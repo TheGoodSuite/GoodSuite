4920015636423b01fb1c58057edf7ca2f7743c1e
Jasper Horn <jasperhorn@gmail.com>
2020-02-07 18:42:05 +0100

    Fix whitespace

--- Good/Service/TypeChecker.php
+++ Good/Service/TypeChecker.php
@@ -12,66 +12,66 @@ class TypeChecker
         {
             return;
         }
-        
+
         if (!is_int($value))
         {
             throw new InvalidParameterException("Value specified is not an int");
         }
-        
+
         if ($value < $minValue)
         {
             throw new InvalidParameterException("Value specified is below the minimum value for this field");
         }
-        
+
         if ($value > $maxValue)
         {
             throw new InvalidParameterException("Value specified is above the maximum value for this field");
         }
     }
-    
+
     public static function checkFloat($value)
     {
         if ($value === null)
         {
             return;
         }
-        
+
         if (!is_int($value) && !is_float($value))
         {
             throw new InvalidParameterException("Value specified is not a float");
         }
     }
-    
+
     public static function checkString($value, $minLength, $maxLength = null)
     {
         if ($value === null)
         {
             return;
         }
-        
+
         if (!is_string($value))
         {
             throw new InvalidParameterException("Value specified is not a string");
         }
-        
+
         if (strlen($value) < $minLength)
         {
             throw new InvalidParameterException("Value specified is shorter than the minimum length for this field");
         }
-        
+
         if ($maxLength !== null && strlen($value) > $maxLength)
         {
             throw new InvalidParameterException("Value specified is longer the maximum length for this field");
         }
     }
-    
+
     public static function checkDateTime($value)
     {
         if ($value === null)
         {
             return;
         }
-        
+
         if (!($value instanceof \DateTime))
         {
             throw new InvalidParameterException("Value specified is not a DateTime");
