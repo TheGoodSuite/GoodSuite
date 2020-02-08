2f268beb5405593d43b85990139686cad3d8c0ec
Jasper Horn <jasperhorn@gmail.com>
2020-02-09 00:13:45 +0100

    Whitespace fixes

--- Good/Manners/Id.php
+++ Good/Manners/Id.php
@@ -11,100 +11,100 @@ class Id implements Storable
 {
     private $base;
     private $id;
-    
+
     private $storage = null;
     private $deleted = false;
-    
+
     public function __construct(Storable $base, Storage $storage, $id)
     {
         $this->base = $base;
         $this->storage = $storage;
         $this->id = $id;
     }
-    
+
     public function setStorage(Storage $storage)
     {
         // Does this function even make sense?
         $this->storage = $storage;
     }
-    
+
     public function delete()
     {
         $this->deleted = true;
         $this->storage->dirtyStorable($this);
     }
-    
+
     public function isDeleted()
     {
         return $this->deleted;
     }
-    
+
     public function isDirty()
     {
         // Only way to make an id dirty is to delete it
         return $this->deleted;
     }
-    
+
     public function setNew($value)
     {
     }
-    
+
     public function isNew()
     {
         return false;
     }
-    
+
     public function setValidationToken(ValidationToken $token)
     {
     }
-    
+
     public function getId()
     {
         return $this->id;
     }
-    
+
     public function setId($value)
     {
         // Does this function even make sense?
         $this->id = id;
     }
-    
+
     public function hasValidId()
     {
         return true;
     }
-    
+
     public function clean()
     {
         // ?
     }
-    
+
     public function getType()
     {
         return $this->base->getType();
     }
-    
+
     public function acceptStorableVisitor(StorableVisitor $visitor)
     {
         // This doesn't need to do anything, because we don't care about the fields of an id
     }
-    
+
     // Note: when the resolver argument becomes optional in the Storage, it should become
     //       optional here as well.
     public function get(Resolver $resolver)
     {
         $collection = $this->storage->getCollection(new EqualTo($this), $resolver);
-        
+
         $first = $collection->getNext();
-        
+
         if ($first === null)
         {
             throw new \Exception("Id not found in storage");
         }
-        
+
         return $first;
     }
-    
+
     public function __get($property)
     {
         if ($property === "id")
