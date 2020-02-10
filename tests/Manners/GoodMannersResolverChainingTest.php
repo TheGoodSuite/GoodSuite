7fb822f4db6df350c08e018dd56a2260fe3ccd35
Jasper Horn <jasperhorn@gmail.com>
2020-02-10 15:50:00 +0100

    [#126] Tests for ordering method resolver chaining

---
+++ tests/Manners/GoodMannersResolverChainingTest.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @runTestsInSeparateProcesses
+ *
+ * Uses a type that has a name and a field that are both a SQL keywords
+ * to test that both table and column names are escaped properly.
+ */
+class GoodMannersArgumentsTest extends \PHPUnit\Framework\TestCase
+{
+    // This could be done just once for all the tests and it would even be necessary
+    // to run the tests in this class in a single process.
+    // However, since we can't run these tests in the same process as those from other
+    // classes (we would have namespace collisions for Storage and SQLStorage)
+    // we have to run every test in different class, and setUpBeforeClass doesn't
+    // play well with that. As such, we'll have to call this function from
+    // setUp instead of having PHPUnit do its magic.
+    public static function _setUpBeforeClass()
+    {
+        // Garbage collector causes segmentation fault, so we disable
+        // for the duration of the test case
+        gc_disable();
+        file_put_contents(dirname(__FILE__) . '/../testInputFiles/PersistenceType.datatype',
+                                                                            "datatype PersistenceType\n" .
+                                                                            "{" .
+                                                                            "   int myInt;\n" .
+                                                                            "   float myFloat;\n".
+                                                                            "   text myText;\n" .
+                                                                            "   datetime myDatetime;\n" .
+                                                                            "}\n");
+
+        $rolemodel = new \Good\Rolemodel\Rolemodel();
+        $schema = $rolemodel->createSchema(array(dirname(__FILE__) . '/../testInputFiles/PersistenceType.datatype'));
+
+        $service = new \Good\Service\Service();
+        $service->compile(array(new \Good\Manners\Modifier\Storable()), $schema, dirname(__FILE__) . '/../generated/');
+
+        require dirname(__FILE__) . '/../generated/PersistenceType.datatype.php';
+
+        require dirname(__FILE__) . '/../generated/PersistenceTypeResolver.php';
+    }
+
+    public static function _tearDownAfterClass()
+    {
+        unlink(dirname(__FILE__) . '/../testInputFiles/PersistenceType.datatype');
+        unlink(dirname(__FILE__) . '/../generated/PersistenceType.datatype.php');
+        unlink(dirname(__FILE__) . '/../generated/PersistenceTypeResolver.php');
+        unlink(dirname(__FILE__) . '/../generated/GeneratedBaseClass.php');
+
+        if (ini_get('zend.enable_gc'))
+        {
+            gc_enable();
+        }
+    }
+
+    public function setUp(): void
+    {
+        $this->_setUpBeforeClass();
+    }
+
+    public function tearDown(): void
+    {
+        $this->_tearDownAfterClass();
+    }
+
+    public function testOrderAscChaining()
+    {
+        $resolver = PersistenceType::resolver();
+
+        $result = $resolver->orderByMyIntAsc();
+
+        $this->assertSame($resolver, $result);
+    }
+
+    public function testOrderDescChaining()
+    {
+        $resolver = PersistenceType::resolver();
+
+        $result = $resolver->orderByMyIntDesc();
+
+        $this->assertSame($resolver, $result);
+    }
+}
+
+?>
